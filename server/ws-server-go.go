9687a58ccb984437ca322d18411dc9a7d43386b9
LeeLin2602 <leelin2602@gmail.com>
2023-06-13 13:01:13 -0400

    Update timeout, single connection, shutdown when disconnecting

--- server/ws-server-go.go
+++ server/ws-server-go.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"io"
+
+	"time"
+	"os"
 	"net/http"
 	"net/url"
 	"github.com/amitbet/vncproxy/logger"
@@ -26,8 +29,16 @@ func (wsServer *WsServer) Listen(urlStr string, handlerFunc WsHandler) {
 		logger.Errorf("error while parsing url: ", err)
 	}
 
+	connected := make(chan string, 5)
+	singleConnection := 0
+
 	http.Handle(url.Path, websocket.Handler(
 		func(ws *websocket.Conn) {
+			if singleConnection == 1 {
+				return;
+			}
+			connected <- "connected"
+			singleConnection = 1
 			path := ws.Request().URL.Path
 			var sessionId string
 			if path != "" {
@@ -36,10 +47,28 @@ func (wsServer *WsServer) Listen(urlStr string, handlerFunc WsHandler) {
 
 			ws.PayloadType = websocket.BinaryFrame
 			handlerFunc(ws, wsServer.cfg, sessionId)
+			connected <- "exiting"
 		}))
 
-	err = http.ListenAndServe(url.Host, nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
+	go func(){
+		err = http.ListenAndServe(url.Host, nil)
+		if err != nil {
+			panic("ListenAndServe: " + err.Error())
+		}
+	}()
+
+	select {
+	case <-connected:
+		logger.Info("Connection Established.")
+		// server.Shutdown(nil)
+		for msg := range connected {
+			logger.Infof("MSG: ", msg)
+			if msg == "exiting" {
+				os.Exit(0)
+			}
+		}
+	case <-time.After(3 * time.Second):
+		logger.Error("Timeout occurred. Stopping listener.")
+		os.Exit(0)
 	}
 }
